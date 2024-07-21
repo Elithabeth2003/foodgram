b93ce996fd6701b572cf117ed4a947c334eca7ae
Liza <Liza.Sivolapova03@yandex.ru>
2024-07-21 17:16:39 +0300

    первый коммит

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -7,71 +7,63 @@ from .models import (
     Recipe,
     RecipeIngredient,
     Favorite,
-    ShoppingCart
+    ShoppingCart,
+    TagRecipe
 )
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'username',
-        'email',
-        'first_name',
-        'last_name',
-        'is_staff',
-        'is_active'
-    )
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    list_filter = ('is_staff', 'is_active', 'date_joined')
-    ordering = ('username',)
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('email', 'username')
 
 
 @admin.register(Subscriptions)
 class SubscriptionsAdmin(admin.ModelAdmin):
-    list_display = ('user', 'author', 'date_added')
+    list_display = ('user', 'author')
     search_fields = ('user__username', 'author__username')
-    list_filter = ('date_added',)
-    ordering = ('-date_added',)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
-    search_fields = ('name', 'slug')
-    ordering = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(TagRecipe)
+class TagRecipeAdmin(admin.ModelAdmin):
+    list_display = ('tag', 'recipe')
 
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
     search_fields = ('name',)
-    ordering = ('name',)
 
 
-class RecipeIngredientInline(admin.TabularInline):
-    model = RecipeIngredient
-    extra = 1
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount')
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author', 'pub_date', 'cooking_time')
-    search_fields = ('name', 'author__username', 'tags__name')
-    list_filter = ('pub_date', 'tags')
-    ordering = ('-pub_date',)
-    inlines = [RecipeIngredientInline]
+    list_display = ('name', 'author')
+    search_fields = ('name', 'author__username')
+    list_filter = ('tags',)
+    readonly_fields = ('favorite_count',)
+
+    def favorite_count(self, obj):
+        return obj.favorite_set.count()
+    favorite_count.short_description = 'Количество добавлений в избранное'
 
 
 @admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'recipe', 'date_added')
+    list_display = ('user', 'recipe')
     search_fields = ('user__username', 'recipe__name')
-    list_filter = ('date_added',)
-    ordering = ('-date_added',)
 
 
 @admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = ('user', 'recipe')
-    search_fields = ('user__username', 'recipe__name')
-    ordering = ('user',)
