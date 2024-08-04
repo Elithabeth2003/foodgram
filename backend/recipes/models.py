e9592c6df25078342cc321208f2970f5e3016589
Liza <Liza.Sivolapova03@yandex.ru>
2024-08-04 13:22:33 +0300

    четвертый коммит1

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -48,9 +48,6 @@ class User(AbstractUser):
         default=None
     )
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
-
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
