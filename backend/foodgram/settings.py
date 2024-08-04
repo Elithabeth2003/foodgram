0e770a671ff936252df3a75a8f153338ec5b30a7
Liza <Liza.Sivolapova03@yandex.ru>
2024-08-04 12:49:30 +0300

    четвертый коммит1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,10 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
-
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+
+#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+
+ALLOWED_HOSTS = ['158.160.64.151', '127.0.0.1', 'localhost', 'cooker.sytes.net']
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost',
