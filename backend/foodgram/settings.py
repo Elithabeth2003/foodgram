5f3b6bf68fe6bc23a7d9a8b5a3438cf6320f90f4
Liza <Liza.Sivolapova03@yandex.ru>
2024-08-04 13:48:27 +0300

    четвертый коммит1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -82,7 +82,7 @@ else:
             'NAME': os.getenv('POSTGRES_DB', 'django'),
             'USER': os.getenv('POSTGRES_USER', 'django'),
             'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-            'HOST': os.getenv('DB_HOST', 'db'),
+            'HOST': os.getenv('DB_HOST', ''),
             'PORT': os.getenv('DB_PORT', 5432),
         }
     }
