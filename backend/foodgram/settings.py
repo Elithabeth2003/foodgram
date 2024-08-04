aa765b6ce5555eb4a270c7dea410f5238cc5f50c
Liza <Liza.Sivolapova03@yandex.ru>
2024-08-04 13:39:33 +0300

    четвертый коммит1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -15,7 +15,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 #ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
-ALLOWED_HOSTS = ['158.160.64.151', '127.0.0.1', 'localhost', 'cooker.sytes.net', 'backend']
+ALLOWED_HOSTS = ['158.160.64.151', '127.0.0.1', 'localhost', 'cooker.sytes.net']
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost',
@@ -82,7 +82,7 @@ else:
             'NAME': os.getenv('POSTGRES_DB', 'django'),
             'USER': os.getenv('POSTGRES_USER', 'django'),
             'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-            'HOST': os.getenv('DB_HOST', ''),
+            'HOST': os.getenv('DB_HOST', 'db'),
             'PORT': os.getenv('DB_PORT', 5432),
         }
     }
