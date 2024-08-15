d429de78cf4dfcda875b86ea56c5f5437717c550
Liza <Liza.Sivolapova03@yandex.ru>
2024-08-15 21:08:14 +0300

    седьмой3

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -34,6 +34,15 @@ class UserSerializer(DjoserUserSerializer):
                 user=request.user, author=user
             ).exists()
         )
+    
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class AvatarSerializer(serializers.ModelSerializer):
