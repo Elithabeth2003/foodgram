8ff6b902518315adeaa2ff19f3bfd005375cb4b6
Liza <Liza.Sivolapova03@yandex.ru>
2024-08-16 00:39:38 +0300

    седьмой7

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -259,12 +259,12 @@ class RecipeCreateSerializer(BaseRecipeSerializer):
             for ingredient in ingredients
         )
 
-    def create(self, initial_data):
+    def create(self, validated_data):
         """Создаёт новый рецепт."""
-        ingredients = self.initial_data.pop('ingredients')
-        tags = self.initial_data.pop('tags')
+        ingredients = validated_data.pop('ingredients')
+        tags = validated_data.pop('tags')
 
-        recipe = Recipe.objects.create(**initial_data)
+        recipe = Recipe.objects.create(**validated_data)
         recipe.tags.set(tags)
         self.set_ingredients(recipe, ingredients)
 
