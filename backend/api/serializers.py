7d3a72e87981827cfe66a0fc4af8e1a7b237c126
Liza <Liza.Sivolapova03@yandex.ru>
2024-08-16 19:12:55 +0300

    восемь8

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -11,6 +11,9 @@ from recipes.models import (
     User,
 )
 from recipes.constants import MIN_INGREDIENT_AMOUNT
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class UserSerializer(DjoserUserSerializer):
@@ -140,7 +143,7 @@ class RecipeRetrieveSerializer(serializers.ModelSerializer):
 
     author = UserSerializer(read_only=True)
     tags = TagSerializer(read_only=True, many=True)
-    ingredients = RecipeIngredientSerializer(many=True)
+    ingredients = serializers.SerializerMethodField()
     is_favorited = serializers.SerializerMethodField()
     is_in_shopping_cart = serializers.SerializerMethodField()
     image = serializers.ImageField()
@@ -160,6 +163,10 @@ class RecipeRetrieveSerializer(serializers.ModelSerializer):
             'is_in_shopping_cart'
         )
         read_only_fields = ('slug_for_short_url', 'author', 'pub_date')
+
+    def get_ingredients(self, recipe):
+        ingredients = RecipeIngredient.objects.filter(recipe=recipe)
+        return RecipeIngredientSerializer(ingredients, many=True).data
 
     def get_is_favorited(self, recipe):
         """Проверяет, добавлен ли рецепт в избранное пользователем."""
@@ -181,7 +188,7 @@ class RecipeRetrieveSerializer(serializers.ModelSerializer):
 class RecipeCreateSerializer(serializers.ModelSerializer):
     """Сериализатор для создания рецепта с использованием id."""
 
-    ingredients = RecipeIngredientCreateSerializer(many=True)
+    ingredients = serializers.SerializerMethodField()
     tags = serializers.PrimaryKeyRelatedField(
         queryset=Tag.objects.all(), many=True
     )
@@ -199,6 +206,10 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
             'id',
         )
         read_only_fields = ('id', 'author')
+
+    def get_ingredients(self, recipe):
+        ingredients = RecipeIngredient.objects.filter(recipe=recipe)
+        return RecipeIngredientSerializer(ingredients, many=True).data
 
     def validate_image(self, value):
         """Проверяет, что поле изображение не пустое."""
@@ -259,7 +270,7 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
         """Добавляет ингредиенты в промежуточную модель."""
         RecipeIngredient.objects.bulk_create(
             RecipeIngredient(
-                ingredient=ingredient['id'],
+                ingredient_id=ingredient['id'],
                 amount=ingredient['amount'],
                 recipe=recipe,
             )
